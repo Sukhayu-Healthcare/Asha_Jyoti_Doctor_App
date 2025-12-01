b12b8ec42586cdb6304f3030e1eda6af010200e4
Shlok-Dubey <160534590+Shlok-Dubey@users.noreply.github.com>
2025-12-01 22:48:17 +0530

    Update default values for doctor name and phone

--- app/src/main/java/com/example/ashajoyti_doctor_app/doctorapp/CHOProfileActivity.kt
+++ app/src/main/java/com/example/ashajoyti_doctor_app/doctorapp/CHOProfileActivity.kt
@@ -110,7 +110,7 @@ class CHOProfileActivity : AppCompatActivity() {
         // Load latest saved values from prefs so changes from EditProfileActivity appear immediately
         val prefs = getSharedPreferences(PREFS, MODE_PRIVATE)
 
-        val name = prefs.getString(KEY_DOCTOR_NAME)
+        val name = prefs.getString(KEY_NAME,"Dr Amit Bhai") ? : "Dr Amit bhai"
         val phone = prefs.getString(KEY_PHONE, "+91-9876543213") ?: "+91-9876543213"
         val avatarUriStr = prefs.getString(KEY_AVATAR_URI, null)
 
